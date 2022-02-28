a2cce91bd12f30271b34b9d7d0d8de44d59aa1e4
brainox <macmartins081@gmail.com>
2022-02-28 22:28:29 +0100

    registered default cell for AppContoller

--- AppStoreJSONApis/Controller/Apps/AppsController.swift
+++ AppStoreJSONApis/Controller/Apps/AppsController.swift
@@ -8,10 +8,23 @@
 import UIKit
 
 class AppsController: BaseListController {
+    let cellId = "id"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         collectionView.backgroundColor = .yellow
+        
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellId)
     }
     
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        cell.backgroundColor = .systemPink
+        return cell
+    }
    
 }
